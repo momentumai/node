3aae0750c10dc08e12b3e3e3ff286251ec36e604
Zoltan Molnar <zoltan@momentum.ai>
2016-08-02 17:45:27 +0100

    ci test

---
+++ bin/deploy_staging.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+MOMENTUM_VERSION=`date '+%Y%m%dt%H%M%S'`
+gcloud preview app deploy node-api.yaml --quiet --version $MOMENTUM_VERSION --no-promote
+git clone -b dev git@github.com:momentumai/static.git
+cd ./static
+cat <<EOF > ./config/version.config.json
+{"endpoint": "https://${MOMENTUM_VERSION}-dot-node-api-dot-momentum-ai.appspot.com/api/"}
+EOF
+git add ./config/version.config.json
+git commit -m "Switched to version $MOMENTUM_VERSION"
+git push origin dev
+cd ..
