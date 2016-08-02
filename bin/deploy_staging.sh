2d6b3a0bd5cd3c4fa9cfc9a7419687e6adeb563f
Zoltan Molnar <zoltan@momentum.ai>
2016-08-02 18:14:22 +0100

    ci test

--- bin/deploy_staging.sh
+++ bin/deploy_staging.sh
@@ -9,6 +9,8 @@ cd ./static
 cat <<EOF > ./config/version.config.json
 {"endpoint": "https://${MOMENTUM_VERSION}-dot-node-api-dot-momentum-ai.appspot.com/api/"}
 EOF
+git config --global user.email "admin@momentum.ai"
+git config --global user.name "Zoltan Molnar"
 git add ./config/version.config.json
 git commit -m "Switched to version $MOMENTUM_VERSION"
 git push origin dev
