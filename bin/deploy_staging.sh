b3658ef2eef35abdbdc03b3a618d0510735d8370
Zoltan Molnar <zoltan@momentum.ai>
2016-08-02 18:02:02 +0100

    ci test

--- bin/deploy_staging.sh
+++ bin/deploy_staging.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 MOMENTUM_VERSION=`date '+%Y%m%dt%H%M%S'`
-cd ..
 gcloud preview app deploy node-api.yaml --quiet --version $MOMENTUM_VERSION --no-promote
+mkdir tmp
+cd tmp
 git clone -b dev git@github.com:momentumai/static.git
 cd ./static
 cat <<EOF > ./config/version.config.json
