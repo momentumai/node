6778d84cd25ccc6b99638c6bbab174cb5755190d
Attila Varga <artit91@gmail.com>
2016-09-01 12:38:57 +0100

    removed crawler and rules scheduler

--- worker/controllers/rules/index.js
+++
@@ -1,8 +0,0 @@
-var logger = require('@lib/logger')('worker/rules'),
-    rulesHandler = require('@lib/worker/rules');
-
-function handler (event, context) {
-    return rulesHandler(event, context, logger);
-}
-
-module.exports.handler = handler;
