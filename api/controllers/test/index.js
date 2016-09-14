aa6eeba1da242ef95e453c6da0bbc2800c360662
Zoltan Molnar <zoltan.molnar@brandvee.com>
2016-09-14 13:36:09 +0100

    fb base backend

--- api/controllers/test/index.js
+++ api/controllers/test/index.js
@@ -1,11 +1,12 @@
 var logger = require('@lib/logger')('/test'),
     dashboardHandler = require('@lib/web/dashboard/filter/facebook'),
-    model = require('@lib/model/fb/adset');
+    model = require('@lib/model/fb/ad');
 
 function handler (event, context) {
     dashboardHandler(event, context, logger, function (local) {
         return model.add(
-            event.data.campaign,
+            event.data.adset,
+            event.data.adcreative,
             event.data.params,
             local.fb.token
         );
