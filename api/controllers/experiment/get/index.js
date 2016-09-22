3823554f841b7260de9a0336bbeadf23b7c4e46d
Zoltan Molnar <zoltan.molnar@brandvee.com>
2016-09-22 15:24:27 +0100

    experiment list + get

--- api/controllers/experiment/get/index.js
+++ api/controllers/experiment/get/index.js
@@ -1,14 +1,12 @@
 var logger = require('@lib/logger')('/experiment/list'),
     dashboardHandler = require('@lib/web/dashboard/filter/facebook'),
-    model = require('@lib/model/experiment/list');
+    model = require('@lib/model/experiment/get');
 
 function handler (event, context) {
     dashboardHandler(event, context, logger, function (local) {
         return model({
             'team_id': local.session.team_id,
-            'cats': local.cats,
-            'limit': event.limit,
-            'offset': event.offset
+            'content_id': event.content_id
         },
             local.fb.token
         );
