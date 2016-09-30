1dc40aec8cbb1e9a44d15464c4ef4ecb686eb404
Attila Varga <artit91@gmail.com>
2016-09-30 12:26:00 +0100

    on/off exp/tests + bugfix

--- api/controllers/experiment/edit/index.js
+++ api/controllers/experiment/edit/index.js
@@ -1,7 +1,6 @@
 var logger = require('@lib/logger')('/experiment/list'),
     dashboardHandler = require('@lib/web/dashboard/filter/facebook'),
-    editModel = require('@lib/model/experiment/edit'),
-    getModel = require('@lib/model/experiment/get');
+    editModel = require('@lib/model/experiment/edit');
 
 function handler (event, context) {
     dashboardHandler(event, context, logger, function (local) {
@@ -11,14 +10,7 @@ function handler (event, context) {
             'data': event.data
         },
             local.fb.token
-        ).then(function () {
-            return getModel({
-                'team_id': local.session.team_id,
-                'content_id': event.content_id
-            },
-                local.fb.token
-            );
-        });
+        );
     });
 }
 
