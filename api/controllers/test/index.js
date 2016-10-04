83478a72648e39cdf7e806cfacfa095892d8a4ec
Zoltan Molnar <zoltan.molnar@brandvee.com>
2016-10-04 08:22:14 +0100

    get winner service

--- api/controllers/test/index.js
+++ api/controllers/test/index.js
@@ -1,15 +1,13 @@
 var logger = require('@lib/logger')('/test'),
     dashboardHandler = require('@lib/web/dashboard/filter/facebook'),
-    model = require('@lib/model/fb/ad');
+    model = require('@lib/model/experiment/getWinner');
 
 function handler (event, context) {
     dashboardHandler(event, context, logger, function (local) {
-        return model.add(
-            event.data.adset,
-            event.data.adcreative,
-            event.data.params,
-            local.fb.token
-        );
+        return model({
+            'team_id': local.session.team_id,
+            'content_id': event.data.content_id
+        }, local.fb.token);
     });
 }
 
